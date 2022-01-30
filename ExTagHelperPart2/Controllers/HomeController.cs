0995a6a3047a8e1796e243a695d51eb471da1376
unknown <faezehjafarpour75@gmail.com>
2022-01-30 22:15:25 +0330

    TagHelper

---
+++ ExTagHelperPart2/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using ExTagHelperPart2.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExTagHelperPart2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            TeamViewModel teamViewModel = new TeamViewModel
+            {
+                Teams = new System.Collections.Generic.List<SelectListItem>()
+                {
+                    new SelectListItem{Value="1" , Text="استقلال"}  ,
+                    new SelectListItem{Value="1" , Text="پرسپولیس"} ,
+                    new SelectListItem{Value="1" , Text="پیکان"}  ,
+                    new SelectListItem{Value="1" , Text="نفت"}  ,
+                    new SelectListItem{Value="1" , Text="ذوب آهن"}  
+                } ,
+
+                SelectListTeamMltipe = new List<SelectListItem>()
+                {
+                    new SelectListItem{Value="1" , Text="استقلال"}  ,
+                    new SelectListItem{Value="1" , Text="پرسپولیس"} ,
+                    new SelectListItem{Value="1" , Text="پیکان"}  ,
+                    new SelectListItem{Value="1" , Text="نفت"}  ,
+                    new SelectListItem{Value="1" , Text="ذوب آهن"}
+                }
+            };
+            return View(teamViewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ExTagHelperPart2/Models/TeamViewModel.cs
@@ -0,0 +1,31 @@
+﻿
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExTagHelperPart2.Models
+{
+    public class TeamViewModel
+    {
+        public string Name { get; set; }
+        public List<SelectListItem> Teams { get; set; }
+        public EnumSuport GetSuport { get; set; }
+        public IEnumerable<string> TeamMltipe { get; set; }
+
+        public List<SelectListItem> SelectListTeamMltipe { get; set; }
+    }
+    public enum EnumSuport
+    {
+        [Display(Name ="فوتبال")]
+        fotball =1,
+
+        [Display(Name = "والیبال")]
+        valibal =2,
+
+        [Display(Name = "بسکتبال")]
+        basketbal =3
+    }
+}
